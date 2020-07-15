273aec93a2be7094bdde707ebf9aa092af028686
Stanko Bebek <stanko.bebek82@gmail.com>
2020-07-15 13:09:18 +0200

    Added vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
-  <div id="app">
-    <DocsGenerator />
-  </div>
+  <v-app>
+    <v-content>
+      <DocsGenerator />
+    </v-content>
+  </v-app>
 </template>
 
 <script>
 import DocsGenerator from '@/components/Docs_generator.vue'
 export default {
   name: 'App',
+
   components: {
     DocsGenerator
-  }
-}
-</script>
+  },
 
-<style>
-</style>
+  data: () => ({
+    //
+  }),
+};
+</script>
