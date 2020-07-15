54e05bcbf45abceff0fb23bbc2a6b326ceb47363
Stanko Bebek <stanko.bebek82@gmail.com>
2020-07-15 20:01:36 +0200

    added pdfmake

--- src/components/Docs_generator.vue
+++ src/components/Docs_generator.vue
@@ -1,10 +1,35 @@
 <template>
-    <h1>Docs generator</h1>
+    <div>
+        <h1>Docs generator</h1>
+
+        <v-btn class="primary" @click="downloadPDF">Download pdf</v-btn>
+    </div>
 </template>
 
 <script>
-export default {
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
 
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+export default {
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+        downloadPDF(){
+            
+            var docDefinition = {
+                content: [
+                    // if you don't need styles, you can use a simple string to define a paragraph
+                    'This is a standard paragraph, using default style'
+                ]
+            };
+
+            pdfMake.createPdf(docDefinition).download();
+        }
+    }
 }
 </script>
 
